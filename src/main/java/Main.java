0c3044fe585917d6db3ec81e7324d36e9c51265e
Gabriel Dinant <gabriel.dinant@migrosonline.ch>
2025-01-22 16:32:57 +0100

    Updated example using class instead of direct styling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import java.io.ByteArrayInputStream;
 import java.io.FileOutputStream;
 import java.nio.charset.StandardCharsets;
 
-import org.docx4j.XmlUtils;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.docx4j.openpackaging.parts.WordprocessingML.AltChunkType;
 
@@ -12,8 +11,22 @@ public class Main {
 
 		var html = """
 			<html>
+				<head>
+					<style>
+						.Heading1 {
+							font-weight: normal;
+							line-height: 1.1;
+							margin-top: 0.2em;
+							margin-bottom: 0.2em;
+							background-color: transparent;
+							color: #404040;
+							font-family: Calibri;
+							font-size: 24pt;
+						}
+					</style>
+				</head>
 				<body>
-					<h1 style="font-weight: normal; line-height: 1.1; margin-top: 0.2em; margin-bottom: 0.2em; background-color: transparent; color: #404040; font-family: Calibri; font-size: 24pt;">H1</h1>
+					<h1 class="Heading1">H1</h1>
 					<h2 style="font-weight: normal; line-height: 1.1; margin-top: 0.2em; margin-bottom: 0.2em; background-color: transparent; color: #404040; font-family: Calibri; font-size: 26pt;">H2</h2>
 				</body>
 			</html>""";
@@ -23,9 +36,6 @@ public class Main {
 
 		mdp.addAltChunk(AltChunkType.Xhtml, new ByteArrayInputStream(html.getBytes(StandardCharsets.UTF_8)));
 		mdp.convertAltChunks();
-
-		System.out.println(
-			XmlUtils.marshaltoString(mlp.getMainDocumentPart().getJaxbElement(), true, true));
 
 		mlp.save(new FileOutputStream("myFile.docx"));
 	}
